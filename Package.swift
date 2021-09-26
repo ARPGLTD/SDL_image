b8ec32390e2d034bd361c7524cf38436e79c2cb8
pcbeard <pcbeard@mac.com>
2021-09-25 17:09:23 -0700

    Move source lists outside of #ifdefs to start port to Windows

--- Package.swift
+++ Package.swift
@@ -6,155 +6,179 @@ import PackageDescription
 struct TargetConfiguration {
     var cflags : [CSetting] = []
     var lflags : [LinkerSetting] = []
+    var libraryType : Product.Library.LibraryType?
     var sourcePaths : [String] = []
     var excludePaths : [String] = []
 }
 
+var pngConfig = TargetConfiguration()
+var jpegConfig = TargetConfiguration()
+var tiffConfig = TargetConfiguration()
+var zlibConfig = TargetConfiguration()
+var imageConfig = TargetConfiguration()
+
 #if os(macOS)
 
-var pngConfig = TargetConfiguration(
-    cflags: [],
-    sourcePaths: [
-        "src/intel/filter_sse2_intrinsics.c",
-        "src/intel/intel_init.c",
-        "src/png.c",
-        "src/pngerror.c",
-        "src/pngget.c",
-        "src/pngmem.c",
-        "src/pngpread.c",
-        "src/pngread.c",
-        "src/pngrio.c",
-        "src/pngrtran.c",
-        "src/pngrutil.c",
-        "src/pngset.c",
-//        "src/pngtest.c",
-        "src/pngtrans.c",
-        "src/pngwio.c",
-        "src/pngwrite.c",
-        "src/pngwtran.c",
-        "src/pngwutil.c",
-//        "src/scripts/intprefix.c",
-//        "src/scripts/prefix.c",
-//        "src/scripts/sym.c",
-//        "src/scripts/symbols.c",
-//        "src/scripts/vers.c",
-    ]
-)
+jpegConfig.cflags = [
+    // .unsafeFlags(["-I./Include"]),  // <jpeglib.h>
+]
 
-var jpegConfig = TargetConfiguration(
-    cflags: [
-        // .unsafeFlags(["-I./Include"]),  // <jpeglib.h>
-    ],
-    sourcePaths: [
-       "src/jaricom.c",
-       "src/jcapimin.c",
-       "src/jcapistd.c",
-       "src/jcarith.c",
-       "src/jccoefct.c",
-       "src/jccolor.c",
-       "src/jcdctmgr.c",
-       "src/jchuff.c",
-       "src/jcinit.c",
-       "src/jcmainct.c",
-       "src/jcmarker.c",
-       "src/jcmaster.c",
-       "src/jcomapi.c",
-       "src/jcparam.c",
-       "src/jcprepct.c",
-       "src/jcsample.c",
-       "src/jctrans.c",
-       "src/jdapimin.c",
-       "src/jdapistd.c",
-       "src/jdarith.c",
-       "src/jdatadst.c",
-       "src/jdatasrc.c",
-       "src/jdcoefct.c",
-       "src/jdcolor.c",
-       "src/jddctmgr.c",
-       "src/jdhuff.c",
-       "src/jdinput.c",
-       "src/jdmainct.c",
-       "src/jdmarker.c",
-       "src/jdmaster.c",
-       "src/jdmerge.c",
-       "src/jdpostct.c",
-       "src/jdsample.c",
-       "src/jdtrans.c",
-       "src/jerror.c",
-       "src/jfdctflt.c",
-       "src/jfdctfst.c",
-       "src/jfdctint.c",
-       "src/jidctflt.c",
-       "src/jidctfst.c",
-       "src/jidctint.c",
-       "src/jmemmgr.c",
-       "src/jmemnobs.c",
-       "src/jquant1.c",
-       "src/jquant2.c",
-       "src/jutils.c",
-       "src/rdbmp.c",
-       "src/rdcolmap.c",
-       "src/rdgif.c",
-       "src/rdppm.c",
-       "src/rdrle.c",
-       "src/rdswitch.c",
-       "src/rdtarga.c",
-       "src/transupp.c",
-       "src/wrbmp.c",
-       "src/wrgif.c",
-       "src/wrppm.c",
-       "src/wrrle.c",
-       "src/wrtarga.c",
-    ]
-)
+zlipConfig.cflags = [
+    // configure sets these.
+    .define("HAVE_UNISTD_H", to: "1"),
+    .define("HAVE_STDARG_H", to: "1")
+]
 
-var tiffConfig = TargetConfiguration(
-    cflags: [
-    ],
-    sourcePaths: [
-//        "src/libtiff/mkg3states.c",
-        "src/libtiff/tif_aux.c",
-        "src/libtiff/tif_close.c",
-        "src/libtiff/tif_codec.c",
-        "src/libtiff/tif_color.c",
-        "src/libtiff/tif_compress.c",
-        "src/libtiff/tif_dir.c",
-        "src/libtiff/tif_dirinfo.c",
-        "src/libtiff/tif_dirread.c",
-        "src/libtiff/tif_dirwrite.c",
-        "src/libtiff/tif_dumpmode.c",
-        "src/libtiff/tif_error.c",
-        "src/libtiff/tif_extension.c",
-        "src/libtiff/tif_fax3.c",
-        "src/libtiff/tif_fax3sm.c",
-        "src/libtiff/tif_flush.c",
-        "src/libtiff/tif_getimage.c",
-        "src/libtiff/tif_jbig.c",
-        "src/libtiff/tif_jpeg.c",
-        "src/libtiff/tif_jpeg_12.c",
-        "src/libtiff/tif_luv.c",
-        "src/libtiff/tif_lzma.c",
-        "src/libtiff/tif_lzw.c",
-        "src/libtiff/tif_next.c",
-        "src/libtiff/tif_ojpeg.c",
-        "src/libtiff/tif_open.c",
-        "src/libtiff/tif_packbits.c",
-        "src/libtiff/tif_pixarlog.c",
-        "src/libtiff/tif_predict.c",
-        "src/libtiff/tif_print.c",
-        "src/libtiff/tif_read.c",
-        "src/libtiff/tif_strip.c",
-        "src/libtiff/tif_swab.c",
-        "src/libtiff/tif_thunder.c",
-        "src/libtiff/tif_tile.c",
-        "src/libtiff/tif_unix.c",
-        "src/libtiff/tif_version.c",
-        "src/libtiff/tif_warning.c",
-        "src/libtiff/tif_webp.c",
+imageConfig.cflags = [
+    .define("SDL_IMAGE_USE_COMMON_BACKEND", to: "1"),
+    .define("LOAD_BMP", to: "1"),
+//        .define("LOAD_GIF", to: "1"),
+    .define("LOAD_JPG", to: "1"),
+//        .define("LOAD_LBM", to: "1"),
+//        .define("LOAD_PCX", to: "1"),
+    .define("LOAD_PNG", to: "1"),
+//        .define("LOAD_PNM", to: "1"),
+//        .define("LOAD_SVG", to: "1"),
+//        .define("LOAD_TGA", to: "1"),
+//        .define("LOAD_TIF", to: "1"),
+//        .define("LOAD_XPM", to: "1"),
+//        .define("LOAD_XV", to: "1"),
+    .unsafeFlags(["-I../libpng/Include"]),  // <png.h>
+]
+
+imageConfig.libraryType = .dynamic
+
+#elseif os(Linux)
+
+#elseif os(Windows)
+
+#endif
+
+pngConfig.sourcePaths = [
+    "src/intel/filter_sse2_intrinsics.c",
+    "src/intel/intel_init.c",
+    "src/png.c",
+    "src/pngerror.c",
+    "src/pngget.c",
+    "src/pngmem.c",
+    "src/pngpread.c",
+    "src/pngread.c",
+    "src/pngrio.c",
+    "src/pngrtran.c",
+    "src/pngrutil.c",
+    "src/pngset.c",
+    "src/pngtrans.c",
+    "src/pngwio.c",
+    "src/pngwrite.c",
+    "src/pngwtran.c",
+    "src/pngwutil.c",
+]
+
+jpegConfig.sourcePaths = [
+   "src/jaricom.c",
+   "src/jcapimin.c",
+   "src/jcapistd.c",
+   "src/jcarith.c",
+   "src/jccoefct.c",
+   "src/jccolor.c",
+   "src/jcdctmgr.c",
+   "src/jchuff.c",
+   "src/jcinit.c",
+   "src/jcmainct.c",
+   "src/jcmarker.c",
+   "src/jcmaster.c",
+   "src/jcomapi.c",
+   "src/jcparam.c",
+   "src/jcprepct.c",
+   "src/jcsample.c",
+   "src/jctrans.c",
+   "src/jdapimin.c",
+   "src/jdapistd.c",
+   "src/jdarith.c",
+   "src/jdatadst.c",
+   "src/jdatasrc.c",
+   "src/jdcoefct.c",
+   "src/jdcolor.c",
+   "src/jddctmgr.c",
+   "src/jdhuff.c",
+   "src/jdinput.c",
+   "src/jdmainct.c",
+   "src/jdmarker.c",
+   "src/jdmaster.c",
+   "src/jdmerge.c",
+   "src/jdpostct.c",
+   "src/jdsample.c",
+   "src/jdtrans.c",
+   "src/jerror.c",
+   "src/jfdctflt.c",
+   "src/jfdctfst.c",
+   "src/jfdctint.c",
+   "src/jidctflt.c",
+   "src/jidctfst.c",
+   "src/jidctint.c",
+   "src/jmemmgr.c",
+   "src/jmemnobs.c",
+   "src/jquant1.c",
+   "src/jquant2.c",
+   "src/jutils.c",
+   "src/rdbmp.c",
+   "src/rdcolmap.c",
+   "src/rdgif.c",
+   "src/rdppm.c",
+   "src/rdrle.c",
+   "src/rdswitch.c",
+   "src/rdtarga.c",
+   "src/transupp.c",
+   "src/wrbmp.c",
+   "src/wrgif.c",
+   "src/wrppm.c",
+   "src/wrrle.c",
+   "src/wrtarga.c",
+]
+
+tiffConfig.sourcePaths = [
+    "src/libtiff/tif_aux.c",
+    "src/libtiff/tif_close.c",
+    "src/libtiff/tif_codec.c",
+    "src/libtiff/tif_color.c",
+    "src/libtiff/tif_compress.c",
+    "src/libtiff/tif_dir.c",
+    "src/libtiff/tif_dirinfo.c",
+    "src/libtiff/tif_dirread.c",
+    "src/libtiff/tif_dirwrite.c",
+    "src/libtiff/tif_dumpmode.c",
+    "src/libtiff/tif_error.c",
+    "src/libtiff/tif_extension.c",
+    "src/libtiff/tif_fax3.c",
+    "src/libtiff/tif_fax3sm.c",
+    "src/libtiff/tif_flush.c",
+    "src/libtiff/tif_getimage.c",
+    "src/libtiff/tif_jbig.c",
+    "src/libtiff/tif_jpeg.c",
+    "src/libtiff/tif_jpeg_12.c",
+    "src/libtiff/tif_luv.c",
+    "src/libtiff/tif_lzma.c",
+    "src/libtiff/tif_lzw.c",
+    "src/libtiff/tif_next.c",
+    "src/libtiff/tif_ojpeg.c",
+    "src/libtiff/tif_open.c",
+    "src/libtiff/tif_packbits.c",
+    "src/libtiff/tif_pixarlog.c",
+    "src/libtiff/tif_predict.c",
+    "src/libtiff/tif_print.c",
+    "src/libtiff/tif_read.c",
+    "src/libtiff/tif_strip.c",
+    "src/libtiff/tif_swab.c",
+    "src/libtiff/tif_thunder.c",
+    "src/libtiff/tif_tile.c",
+    "src/libtiff/tif_unix.c",
+    "src/libtiff/tif_version.c",
+    "src/libtiff/tif_warning.c",
+    "src/libtiff/tif_webp.c",
 //        "src/libtiff/tif_win32.c",
-        "src/libtiff/tif_write.c",
-        "src/libtiff/tif_zip.c",
-        "src/libtiff/tif_zstd.c",
+    "src/libtiff/tif_write.c",
+    "src/libtiff/tif_zip.c",
+    "src/libtiff/tif_zstd.c",
 //        "src/port/dummy.c",
 //        "src/port/getopt.c",
 //        "src/port/lfind.c",
@@ -164,82 +188,51 @@ var tiffConfig = TargetConfiguration(
 //        "src/port/strtoll.c",
 //        "src/port/strtoul.c",
 //        "src/port/strtoull.c",
-    ]
-)
+]
 
-var zlibConfig = TargetConfiguration(
-    cflags: [
-        // configure sets these.
-        .define("HAVE_UNISTD_H", to: "1"),
-        .define("HAVE_STDARG_H", to: "1")
-    ],
-    sourcePaths: [
-        "src/adler32.c",
-        "src/compress.c",
+zlibConfig.sourcePaths = [
+    "src/adler32.c",
+    "src/compress.c",
 //        "src/contrib/blast/blast.c",
 //        "src/crc32.c",
-        "src/gzclose.c",
-        "src/gzlib.c",
-        "src/gzread.c",
-        "src/gzwrite.c",
-        "src/infback.c",
-        "src/inffast.c",
-        "src/inflate.c",
-        "src/inftrees.c",
-        "src/trees.c",
-        "src/uncompr.c",
-        "src/zutil.c",
-    ]
-)
+    "src/gzclose.c",
+    "src/gzlib.c",
+    "src/gzread.c",
+    "src/gzwrite.c",
+    "src/infback.c",
+    "src/inffast.c",
+    "src/inflate.c",
+    "src/inftrees.c",
+    "src/trees.c",
+    "src/uncompr.c",
+    "src/zutil.c",
+]
 
-var imageConfig = TargetConfiguration(
-    cflags: [
-        .define("SDL_IMAGE_USE_COMMON_BACKEND", to: "1"),
-        .define("LOAD_BMP", to: "1"),
-//        .define("LOAD_GIF", to: "1"),
-        .define("LOAD_JPG", to: "1"),
-//        .define("LOAD_LBM", to: "1"),
-//        .define("LOAD_PCX", to: "1"),
-        .define("LOAD_PNG", to: "1"),
-//        .define("LOAD_PNM", to: "1"),
-//        .define("LOAD_SVG", to: "1"),
-//        .define("LOAD_TGA", to: "1"),
-//        .define("LOAD_TIF", to: "1"),
-//        .define("LOAD_XPM", to: "1"),
-//        .define("LOAD_XV", to: "1"),
-        .unsafeFlags(["-I../libpng/Include"]),  // <png.h>
-    ],
-    sourcePaths: [
-        "src/IMG.c",
-        "src/IMG_bmp.c",
-        "src/IMG_gif.c",
-        "src/IMG_jpg.c",
-        "src/IMG_lbm.c",
-        "src/IMG_pcx.c",
-        "src/IMG_png.c",
-        "src/IMG_pnm.c",
-        "src/IMG_svg.c",
-        "src/IMG_tga.c",
-        "src/IMG_tif.c",
-        "src/IMG_webp.c",
-        "src/IMG_WIC.c",
-        "src/IMG_xcf.c",
-        "src/IMG_xpm.c",
-        "src/IMG_xv.c",
-        "src/IMG_xxx.c",
-    ])
-
-#else
-
-var sdlConfig = TargetConfiguration()
-
-#endif
+imageConfig.sourcePaths = [
+    "src/IMG.c",
+    "src/IMG_bmp.c",
+    "src/IMG_gif.c",
+    "src/IMG_jpg.c",
+    "src/IMG_lbm.c",
+    "src/IMG_pcx.c",
+    "src/IMG_png.c",
+    "src/IMG_pnm.c",
+    "src/IMG_svg.c",
+    "src/IMG_tga.c",
+    "src/IMG_tif.c",
+    "src/IMG_webp.c",
+    "src/IMG_WIC.c",
+    "src/IMG_xcf.c",
+    "src/IMG_xpm.c",
+    "src/IMG_xv.c",
+    "src/IMG_xxx.c",
+]
 
 let package = Package(
     name: "SDL_image",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "SDL_image", type: .dynamic, targets: ["SDL_image"]),
+        .library(name: "SDL_image", type: imageConfig.libraryType, targets: ["SDL_image"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -289,11 +282,11 @@ let package = Package(
             publicHeadersPath: "Include",
             cSettings: imageConfig.cflags,
             linkerSettings: imageConfig.lflags
-            ),
+        ),
         .testTarget(
             name: "SDL_image_Tests",
             dependencies: ["SDL_image", "SDL"],
                 resources: [.copy("image.png")]
-            ),
+        ),
     ]
 )
